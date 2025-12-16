8861a409e3bd68aaa81ef4232fc5a26bdf3c67b7
faraaz777 <129242477+faraaz777@users.noreply.github.com>
2025-12-16 22:08:34 +0530

    whom we served

--- app/(main)/whom-we-serve/[slug]/page.js
+++ app/(main)/whom-we-serve/[slug]/page.js
@@ -469,6 +469,7 @@ export default function CategoryPage() {
         </div>
       </section>
 
+
       {/* Partners Section */}
       <section className="py-16 md:py-24 bg-[#F5F5F5]">
         <div className="max-w-7xl mx-auto px-4 md:px-8 lg:px-12">
